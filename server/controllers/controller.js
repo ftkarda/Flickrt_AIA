fda6f771694a61eecfdd65a13190d2c15f95ec4f
arda <fatikaardha@gmail.com>
2022-04-30 02:22:03 +0800

    update server, setup layout client + trying fetch from backend

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -3,11 +3,10 @@ const axios = require("axios");
 class Controller {
   static async getPhotos(req, res, next) {
     try {
-      const { tag } = req.params;
       const response = await axios.get(
-        `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=f19e81c63412c0ea3ef8d35a67ca1833&tags=${tag}&format=json&nojsoncallback=1&api_sig=0ce86e4fa3abe82459af353b0b1c3df7`
+        `https://www.flickr.com/services/feeds/photos_public.gne?format=json&tags=cats`
       );
-      res.status(200).json(response.data)
+      res.status(200).json({data: response.data} )
     } catch (error) {
       console.log(error);
     }
